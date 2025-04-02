f2a8b0df567701e71c7d13095c50f88a0a452926
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-04-02 14:26:03 +0200

    add further animations to homepage

--- src/assets/js/homepage.js
+++ src/assets/js/homepage.js
@@ -1,10 +1,13 @@
-// This file contains all JS related to the homepage
+/* This file contains all JS related to the homepage */
 
 // Reusable function to observe any element with a custom callback function. Intended for animations.
 // Default option is to run callback whenever 20% of the element is within the viewport, then unobserve.
 function observeElement(selector, callback, options = { threshold: 0.2 }) {
     const element = document.querySelector(selector);
-    if (!element) return;
+    if (!element) {
+        console.log(`[!] observeElement(): selector ${selector} not found.`);
+        return;
+    }
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach((entry) => {
@@ -19,9 +22,9 @@ function observeElement(selector, callback, options = { threshold: 0.2 }) {
 }
 
 // Animations for .hero-header
-function animateHeroHeader(element) {
-    const tagWrapper = element.querySelector("#hero .tag-wrapper");
-    const heading = element.querySelector("#hero .heading");
+function animateHeroHeader(heroHeader) {
+    const tagWrapper = heroHeader.querySelector("#hero .tag-wrapper");
+    const heading = heroHeader.querySelector("#hero .heading");
 
     if (tagWrapper) {
         tagWrapper.classList.add("animate");
@@ -32,8 +35,55 @@ function animateHeroHeader(element) {
     }
 }
 
+// Animations for .text-images
+function animateTextImages(textImages) {
+    const spanOne = textImages.querySelector(".span.one");
+    const spanTwo = textImages.querySelector(".span.two");
+    const spanThree = textImages.querySelector(".span.three");
+
+    // We do not need to check if the function parameter exists.
+    // This check is done in the observeElement() function.
+    textImages.classList.add("animate");
+
+    if (spanOne) {
+        spanOne.classList.add("animate");
+    }
+
+    if (spanTwo) {
+        spanTwo.classList.add("animate");
+    }
+
+    if (spanThree) {
+        spanThree.classList.add("animate");
+    }
+}
+
+// Animations for .big-numbers-card.<number>
+function animateBigNumbersCard(bigNumbersCard) {
+    // Since we are not querying for any new elements,
+    // there is no need to check if the element exists.
+    // This check is done in the observeElement() function.
+    bigNumbersCard.classList.add("animate");
+}
+
 function initializeAnimations() {
     observeElement(".hero-header", animateHeroHeader);
+    observeElement("#brand-statements .text-images", animateTextImages);
+    observeElement(
+        "#brand-statements .big-numbers-card.one",
+        animateBigNumbersCard,
+        (options = { threshold: 1 }),
+    );
+    observeElement(
+        "#brand-statements .big-numbers-card.two",
+        animateBigNumbersCard,
+        (options = { threshold: 1 }),
+    );
+    observeElement(
+        "#brand-statements .big-numbers-card.three",
+        animateBigNumbersCard,
+        (options = { threshold: 1 }),
+    );
 }
 
 // Initialize all animations on page load
