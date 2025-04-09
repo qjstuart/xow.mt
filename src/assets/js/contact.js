9140c5d4c4f2d056172bb0169bf9e1fb90c746ba
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-04-09 18:34:27 +0200

    perform form submission via ajax

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -17,6 +17,49 @@ function animateHeroHeader(heroHeader) {
     }
 }
 
+// AJAX Contact Form submission
+async function handleSubmit(event) {
+    console.log("Form submission started");
+    // Prevent default form submission behaviour
+    // This is important to prevent the page from reloading
+    // so we can show our own success/error messages via HTML
+    event.preventDefault();
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    const formFailMessage = document.querySelector(".error-message.w-form-fail");
+    const formSuccessMessage = document.querySelector(
+        ".success-message.w-form-done",
+    );
+
+    // fetch("/", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //     body: new URLSearchParams(formData).toString(),
+    // })
+    //     .then(() => console.log("Form successfully submitted"))
+    //     .catch((error) => alert(error));
+
+    try {
+        const response = await fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        });
+
+        if (!response.ok) {
+            formFailMessage.classList.add("visible");
+            throw new Error("Form submission failed");
+        } else {
+            formSuccessMessage.classList.add("visible");
+            console.log("Form successfully submitted");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
 // Function to initialize all animations
 function initializeAnimations() {
     // Observe elements in #hero section
@@ -25,3 +68,7 @@ function initializeAnimations() {
 
 // Initialize all animations on page load
 document.addEventListener("DOMContentLoaded", initializeAnimations);
+// Initialize Contact Form submission handler
+document
+    .querySelector("form#email-form")
+    .addEventListener("submit", handleSubmit);
