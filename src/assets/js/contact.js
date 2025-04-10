e5722ffce6bf3375cbed1a0525b3e2baf482f744
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-04-10 11:12:27 +0200

    code commenting

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -22,25 +22,23 @@ async function handleSubmit(event) {
     console.log("Form submission started");
     // Prevent default form submission behaviour
     // This is important to prevent the page from reloading
-    // so we can show our own success/error messages via HTML
+    // so we can show our own success/fail messages via HTML
     event.preventDefault();
 
+    // Get the form element and form data
     const myForm = event.target;
     const formData = new FormData(myForm);
 
-    const formFailMessage = document.querySelector(".error-message.w-form-fail");
+    // Get html elements for form submission success/fail
     const formSuccessMessage = document.querySelector(
         ".success-message.w-form-done",
     );
+    const formFailMessage = document.querySelector(
+        ".error-message.w-form-fail",
+    );
 
-    // fetch("/", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //     body: new URLSearchParams(formData).toString(),
-    // })
-    //     .then(() => console.log("Form successfully submitted"))
-    //     .catch((error) => alert(error));
-
+    // Submit form via HTTP POST request to "/". Netlify processes this
+    // request and forwards the form data to an email address of choice.
     try {
         const response = await fetch("/", {
             method: "POST",
@@ -49,14 +47,18 @@ async function handleSubmit(event) {
         });
 
         if (!response.ok) {
+            // Show form submission fail HTML element
             formFailMessage.classList.add("visible");
             throw new Error("Form submission failed");
         } else {
+            // Show form submission success HTML element
             formSuccessMessage.classList.add("visible");
-            console.log("Form successfully submitted");
         }
     } catch (error) {
-        console.error("Error:", error);
+        // For any other reason for failure,
+        // we also show form submission fail HTML element.
+        formFailMessage.classList.add("visible");
+        console.error("Form submission failed: ", error);
     }
 }
 
