c3c4f2907d1cfd638851c4b57b1701b5dfd0c6b8
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-04-11 12:04:45 +0200

    code formatting

--- src/assets/js/observe.js
+++ src/assets/js/observe.js
@@ -1,6 +1,10 @@
 // Reusable function to observe any element with a custom callback function. Intended for animations.
 // Default option is to run callback whenever 20% of the element is within the viewport, then unobserve.
-export function observeElement(selector, callback, options = { threshold: 0.2 }) {
+export function observeElement(
+    selector,
+    callback,
+    options = { threshold: 0.2 },
+) {
     const element = document.querySelector(selector);
     if (!element) {
         console.log(`[!] observeElement(): selector ${selector} not found.`);
