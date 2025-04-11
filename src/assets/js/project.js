a2e6973bc932d2a7d62a1b7c66e395b47b015ffe
DESKTOP-KV0O2DH\Quentin <quentinqjs@gmail.com>
2025-04-11 12:04:33 +0200

    animate hero image on project details page

---
+++ src/assets/js/project.js
@@ -0,0 +1,30 @@
+/* This file contains all JS related to the project details page (portfolio/project-name/) */
+
+// Import resuable function for observing any element
+import { observeElement } from "./observe.js";
+
+// Animations for all .mask-frame elements
+function animateBlogImageWrapper(blogImageWrapper) {
+    // Animate .mask-frame, the color fill which covers the project hero image
+    const maskFrame = blogImageWrapper.querySelector(".mask-frame");
+    if (maskFrame) {
+        maskFrame.classList.add("animate");
+    }
+
+    // Animate the project hero image (zoom-out effect)
+    const image = blogImageWrapper.querySelector("img");
+    if (image) {
+        image.classList.add("animate");
+    }
+}
+
+// Function to initialize all animations
+function initializeAnimations() {
+    observeElement(
+        "#project-hero-image .blog-image-wrapper",
+        animateBlogImageWrapper,
+    );
+}
+
+// Initialize all animations on page load
+document.addEventListener("DOMContentLoaded", initializeAnimations);
